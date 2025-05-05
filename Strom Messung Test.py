737285dbd0f3e42ee3e0804de2235985f4e0f849
Fynn0705 <Fynn_Bremer@outlook.de>
2025-05-05 10:00:08 +0200

    05.05.2025

--- Strom Messung Test.py
+++
@@ -1,4 +0,0 @@
-# Angenommen, du hast den INA219-Treiber zur Verfügung
-from ina219 import INA219
-i2c = I2C(0, scl=Pin(5), sda=Pin(4))  # Verwende die entsprechenden Pins für ESP32
-ina = INA219(i2c)
